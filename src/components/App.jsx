3538f6e923c4fe92646a3671543dc7898d0bd463
Tianyao Ma <tianyao.ma333@gmail.com>
2021-09-02 16:02:13 -0400

    implemented delete note function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,18 +14,25 @@ function App() {
         });
     }
 
-    function deleteItem(event) {
-
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            })
+        })
     }
 
     return (
         <div>
             <Header />
             <CreateArea onAdd={addNote} />
-            {notes.map((noteItem) => {
-                return <Note 
-                title={noteItem.title}
-                content={noteItem.content}
+            {notes.map((noteItem, index) => {
+                return <Note
+                    key={index}
+                    id={index}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    onDelete={deleteNote}
                 />
             })}
             <Footer />
